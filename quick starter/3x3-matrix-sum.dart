bff0524b76eebcba1389b59ea523d834003d8872
Ashupal Deora <143180848+Ashupaldeora@users.noreply.github.com>
2024-01-13 22:27:07 +0530

    Add files via upload

---
+++ quick starter/3x3-matrix-sum.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+void main()
+{
+  List matrix1=[[1,2,3],[4,5,6],[7,8,9]];
+  var f;
+  // stdout.write('Enter dimension of 1st matrix :');
+  // int dimension = int.parse(stdin.readLineSync()!);
+
+  // for(int i=0; i<dimension; i++)
+  // {
+  //   for(int j=0; j<dimension; j++)
+  //   {
+  //     stdout.write('matrix[$i][$j] : ');
+      
+  //     matrix1.add(int.parse(stdin.readLineSync()!));
+  //   }
+  // }
+   List matrix2=[[1,2,3],[4,5,6],[7,8,9]];
+ 
+
+  // for(int i=0; i<dimension; i++)
+  // {
+  //   for(int j=0; j<dimension; j++)
+  //   {
+  //     stdout.write('matrix[$i][$j] : ');
+      
+  //     matrix2.add(int.parse(stdin.readLineSync()!));
+      
+  //   }
+  // }
+ var temp=f.addition(matrix1,matrix2);
+ print(temp);
+
+
+}
