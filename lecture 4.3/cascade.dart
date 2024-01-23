875383a8a46c19151bb4e42fe84c3ed572f960b2
Ashupal Deora <143180848+Ashupaldeora@users.noreply.github.com>
2024-01-23 10:18:57 +0530

    Create cascade.dart

---
+++ lecture 4.3/cascade.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+class FeetInch
+{
+  int feet=0,inch=0;
+
+  void set()
+  {
+    stdout.write("Feet : ");
+    feet = int.parse(stdin.readLineSync()!);
+    stdout.write("Inch : ");
+    inch = int.parse(stdin.readLineSync()!);
+  }
+
+  void get()
+  {
+    feet+=inch~/12;
+    inch -= (inch~/12)*12;
+
+    print("Feet : $feet");
+    print("Inch : $inch");
+
+  }
+}
+void main()
+{
+  FeetInch fi=FeetInch();
+  fi..set()..get();
+}
